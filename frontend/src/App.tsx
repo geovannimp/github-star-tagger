6de2c28bd98ffd432fa1616843b78e5c38d1ac37
Geovanni Pacheco <geovanni.m.pacheco@gmail.com>
2018-05-03 09:59:30 -0300

    Inital config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+            </div>
+        );
+    }
+}
+
+export default App;
